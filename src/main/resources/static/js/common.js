cf3952b337df411a4351ba1d01509ebb093995d3
tcw <1483674736@qq.com>
2019-11-03 16:29:14 +0800

    完成获取订单详情的前端js代码

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -6,3 +6,20 @@ function goto(url) {
         window.location.href=url;
 
 }
+
+/**
+ * 获取URL中的信息
+ * @param name
+ * @returns {*}
+ * @constructor
+ */
+function GetPar(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r != null){
+        return decodeURIComponent(r[2]);
+    }
+    return null;
+}
+
+
